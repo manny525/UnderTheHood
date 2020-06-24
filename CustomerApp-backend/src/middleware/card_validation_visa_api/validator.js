bb249ea953b91adf677bc98ea3fcaabc7cc8374a
AshutoshAgrawal31 <60539711+AshutoshAgrawal31@users.noreply.github.com>
2020-06-24 14:32:16 +0530

    card_validation_api

---
+++ CustomerApp-backend/src/middleware/card_validation_visa_api/validator.js
@@ -0,0 +1,31 @@
+// 'use strict';
+var api = require('./src/payment').payment_account_validation_api;
+var authCredentials = require('./credentials.json');
+
+var payment_account_validation_api = new api(authCredentials);
+
+const validate = async(req,res,next)=>{
+    try{
+        console.log(req.body);
+        const payment = await payment_account_validation_api.cardvalidation(getParameters(req))
+        next()
+    }catch(e){
+        res.status(400).send({error:e.body.errorMessage})
+    }
+}
+
+function getParameters(req) {
+    var parameters = {
+        "x-client-transaction-id": "{enter appropriate value}",
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    };
+    parameters.payload = {    
+        "primaryAccountNumber": req.body.number,  
+        "cardExpiryDate": req.body.date,  
+        "cardCvv2Value": req.body.cvv,  
+    }
+    return parameters;
+}
+
+module.exports = validate
--- CustomerApp-backend/src/routers/loyalty.js
+++ CustomerApp-backend/src/routers/loyalty.js
@@ -3,6 +3,8 @@ const Loyalty = require('./../models/loyalty')
 require('./../db/mongoose')
 const router = new express.Router()
 const auth = require('./../middleware/auth')
+const  randomize = require('randomatic')
+const mongoose = require('mongoose')
 
 router.post('/add/loyalty',auth,async(req,res)=>{
     try{
@@ -14,6 +16,37 @@ router.post('/add/loyalty',auth,async(req,res)=>{
         res.send()
     }catch(error){
         res.status(400).send({error})
+    }
+})
+
+router.get('/get/promocode',auth,async(req,res)=>{
+    try{
+        const loyalty = await Loyalty.findOne({customer:req.user._id,merchant:mongoose.Types.ObjectId(req.body.merchant)})
+        
+        if(!loyalty.promocode){
+            if(loyalty.points<100){
+                return res.status(400).send({error:'points less than 100'})
+            }
+            loyalty.promocode=randomize('Aa0',10)
+            await loyalty.save()    
+        }
+        res.send(loyalty)
+    }catch(error){
+        res.status(400).send({error:'my error'})
+    }
+})
+
+router.delete('/promocode',auth,async(req,res)=>{
+    try{
+        const loyalty = await Loyalty.findOne({customer:req.user._id,merchant:mongoose.Types.ObjectId(req.body.merchant)})
+        if(loyalty.promocode){
+            loyalty.points = loyalty.points - 100
+        }
+        loyalty.promocode = undefined
+        await loyalty.save()
+        res.send(loyalty)
+    }catch(e){
+        res.status(400).send({error:e})
     }
 })
 
